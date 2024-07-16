a5a20009ed3aec3d6e04fcf5ab29ee77c1da560d
muyiwa99 <muyiwa99@live.co.uk>
2024-07-16 11:15:50 +0100

    JS Update

--- dist/JS/script.js
+++ dist/JS/script.js
@@ -18,78 +18,87 @@ const shortlinkContainer = document.querySelector("link-container_link-box");
 const body = document.getElementsByTagName("BODY")[0];
 
 document.addEventListener("DOMContentLoaded", () => {
-  initializeApp();
+	initializeApp();
 
-  shortenButton.addEventListener("click", async function () {
-    // let link = inputLink.value.trim();
-    // console.log("Link input value:", link);
-    // //   console.log("1");
-    // if (validateLink(link)) {
-    //   await shortenLink(link);
-    //   appendLinkContainer();
-    //   console.log(link);
-    // } else {
-    //   displayValidationError();
-    // }
+	shortenButton.addEventListener("click", async function () {
+		// let link = inputLink.value.trim();
+		// console.log("Link input value:", link);
+		// //   console.log("1");
+		// if (validateLink(link)) {
+		//   await shortenLink(link);
+		//   appendLinkContainer();
+		//   console.log(link);
+		// } else {
+		//   displayValidationError();
+		// }
 
-    let linkInput = document.getElementById("linkInput");
-    if (!linkInput) {
-      console.error("Link input element not found");
-      return;
-    }
-    let link = linkInput.value.trim();
-    console.log("Link input value:", link); // Debug: Check the value of the link input
+		let linkInput = document.getElementById("linkInput");
+		if (!linkInput) {
+			console.error("Link input element not found");
+			return;
+		}
+		let link = linkInput.value.trim();
+		console.log("Link input value:", link); // Debug: Check the value of the link input
 
-    if (validateLink(link)) {
-      await shortenLink(link);
-    } else {
-      displayValidationError();
-    }
-  });
+		if (validateLink(link)) {
+			await shortenLink(link);
+		} else {
+			displayValidationError();
+		}
+	});
 });
 // Initializes the appliccation
 const initializeApp = function () {
-  //   loadLocalStorageLinks();
-  //   displayLocalStorageLinks();
+	//   loadLocalStorageLinks();
+	//   displayLocalStorageLinks();
 };
 
 const validateLink = function (link) {
-  if (!link) {
-    console.error("Link is undefined or empty"); // Debug: Link is undefined or empty
-    return false;
-  }
-  return link.startsWith("http://") || link.startsWith("https://");
+	if (!link) {
+		inputLink.style.border = "3px solid red";
+
+		inputLink.classList.add("link-container_inputE");
+
+		inputLink.placeholder = " Please provide a link here!";
+
+		console.error("Link is undefined or empty"); // Debug: Link is undefined or empty
+		return false;
+	}
+	return link.startsWith("http://") || link.startsWith("https://");
 };
 
 const shortenLink = async function (link) {
-  try {
-    const response = await fetch("https://shrtlnk.dev/api/v2/link", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ orinalURL: link }),
-    });
+	try {
+		const response = await fetch("https://cleanuri.com/api/v1/shorten", {
+			mode: "no-cors",
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({ orinalURL: link }),
+		});
+		console.log(response);
 
-    const data = await response.json();
-    if (data.shortURL) {
-      let shortenedLink = data.shortURL;
-      appendLinkContainer(link, shortenedLink);
-      saveLinkToLocalStorage(link, shortenedLink);
-    } else {
-      console.error("Error Shortening link:", data);
-      displayError("Error shortening the link. Please try again");
-    }
-  } catch {
-    console.error(error);
-    displayError("Error shortening the link. Please try again");
-  }
+		const data = await response.json();
+		if (data.shortURL) {
+			let shortenedLink = data.shortURL;
+			appendLinkContainer(link, shortenedLink);
+			saveLinkToLocalStorage(link, shortenedLink);
+		} else {
+			console.error("Error Shortening link:", data);
+			displayError("Error shortening the link. Please try again");
+		}
+	} catch {
+		console.error(error);
+		displayError("Error shortening the link. Please try again");
+	}
 };
 
 const appendLinkContainer = function (orignialLink, shortenedLink) {
-  shortlinkContainer.innerHTML = "";
+	shortlinkContainer.innerHTML = "";
 
-  const html = `<div class="link-container_link-box">
+	const html = `<div class="link-container_link-box">
 				<p class="old-link">${orignialLink}</p>
 				<hr />
 				<div class="new">
@@ -99,20 +108,20 @@ const appendLinkContainer = function (orignialLink, shortenedLink) {
 				</div>
 			</div>`;
 
-  inputLink.insertAdjacentElement("afterend", html);
+	inputLink.insertAdjacentElement("afterend", html);
 };
 
 function displayValidationError() {
-  alert("Invalid link. Please make sure it starts with http:// or https://");
+	alert("Invalid link. Please make sure it starts with http:// or https://");
 }
 
 function saveLinkToLocalStorage(originalLink, shortenedLink) {
-  let links = JSON.parse(localStorage.getItem("links")) || [];
-  links.push({ original: originalLink, shortened: shortenedLink });
-  localStorage.setItem("links", JSON.stringify(links));
+	let links = JSON.parse(localStorage.getItem("links")) || [];
+	links.push({ original: originalLink, shortened: shortenedLink });
+	localStorage.setItem("links", JSON.stringify(links));
 }
 
 function loadLinksFromLocalStorage() {
-  let links = JSON.parse(localStorage.getItem("links")) || [];
-  links.forEach((link) => appendLinkContainer(link.original, link.shortened));
+	let links = JSON.parse(localStorage.getItem("links")) || [];
+	links.forEach((link) => appendLinkContainer(link.original, link.shortened));
 }
